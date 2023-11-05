a646e116ff734525d5904fd0a190bf3da1283bac
Al-Fetyani <abdallahfetyani2@gmail.com>
2023-11-05 12:22:06 +0300

    Fixing time issues

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from docx import Document
-from email.utils import formataddr
 import pandas as pd
 import smtplib
+from email.utils import formataddr
+from docx import Document
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import threading
+import time
+import argparse
+import getpass
 
 
-def fill_invitation(template_path, output_path, data):
+def fill_invitation(template_path, data):
     doc = Document(template_path)
 
     for p in doc.paragraphs:
@@ -19,42 +23,76 @@ def fill_invitation(template_path, output_path, data):
     return doc_content
 
 
-def generate_invitation(csv_path, template_path):
-    df = pd.read_csv(csv_path)
-
-    for _, row in df.iterrows():
-        full_name = f"{row['first_name']}_{row['last_name']}"
-        data = {
-            "[Salutation]": row["salutation"],
-            "[First Name]": row["first_name"],
-            "[Last Name]": row["last_name"],
-            "[Last Contacted]": row["last_contacted"],
-            "[Company Name]": row["company"],
-        }
-        output_path = f"{full_name} invitation.docx"
-        yield fill_invitation(template_path, output_path, data)
-
-
-def send_email(csv_path, subject, username, password, body):
+def send_email(to_email, subject, username, password, body):
     msg = MIMEMultipart()
     msg["Subject"] = subject
     msg["From"] = formataddr(("Al-Fetyani", username))
     msg.attach(MIMEText(body, "plain", "utf-8"))
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(username, password)
-    df = pd.read_csv(csv_path)
-    for idx, row in df.iterrows():
-        email = row["email"]
-        server.sendmail(username, email, msg.as_string(), subject)
-    server.quit()
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(username, password)
+            server.sendmail(username, to_email, msg.as_string())
+        print(f"Email sent to: {to_email}")
+    except Exception as e:
+        print(f"Error sending email to {to_email}: {e}")
+
+
+def send_emails(emails, subject, username, password):
+    threads = []
+    for body, to_email in emails:
+        thread = threading.Thread(
+            target=send_email, args=(to_email, subject, username, password, body)
+        )
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
 
 if __name__ == "__main__":
     csv_path = "contacts.csv"
     template_path = "template.docx"
     subject = "Invite to Event in 2023"
-    username = "abdallahfetyani2@gmail.com"
-    password = "vawr zjoj epoa jntx"
-    for inv in generate_invitation(csv_path, template_path):
-        send_email(csv_path, subject, username, password, body=inv)
+
+    parser = argparse.ArgumentParser(
+        description="Send emails using a CSV file and a Word document template."
+    )
+
+    parser.add_argument("--email", required=True, help="Your email address.")
+    parser.add_argument(
+        "--password",
+        required=True,
+        help="Your email password. If not provided, you'll be prompted to enter it securely.",
+    )
+
+    args = parser.parse_args()
+
+    if args.password is None:
+        args.password = getpass.getpass(prompt="Enter your email password: ")
+    start_time = time.time()
+
+    emails_to_send = [
+        (
+            fill_invitation(
+                template_path,
+                {
+                    "[Salutation]": row["salutation"],
+                    "[First Name]": row["first_name"],
+                    "[Last Name]": row["last_name"],
+                    "[Last Contacted]": row["last_contacted"],
+                    "[Company Name]": row["company"],
+                },
+            ),
+            row["email"],
+        )
+        for _, row in pd.read_csv(csv_path).iterrows()
+    ]
+
+    send_emails(emails_to_send, subject, args.email, args.password)
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Total time taken: {elapsed_time:.2f} seconds")
